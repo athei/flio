738af4f2e3616d8e64d4ffd7bf56cd32bc9de44d
Alexander Theißen <alex@theissen.io>
2019-03-03 23:22:21 +0100

    Added severity and facility functions to NTStatus

--- smb2-packet/src/ntstatus.rs
+++ smb2-packet/src/ntstatus.rs
@@ -1,16 +1,66 @@
 use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 
 impl NTStatus {
     pub fn is_success(self) -> bool {
-        // success is indicated by the 2 msb bits
-        ((self as u32) & (0b11 << 30)) == 0
+        if let Severity::Success = self.severity() {
+            return true;
+        }
+        false
+    }
+
+    pub fn severity(self) -> Severity {
+        let severity = (self as u32) >> 30;
+        // wont fail because we only put in 2 bytes
+        Severity::from_u32(severity).unwrap()
+    }
+
+    pub fn facility(self) -> Option<Facility> {
+        let facility = ((self as u32) >> 16) & 0xFFF;
+        Facility::from_u32(facility)
     }
 }
 
-impl From<NTStatus> for bool {
-    fn from(status: NTStatus) -> Self {
-        status.is_success()
-    }
+#[repr(u8)]
+#[derive(FromPrimitive, Clone, Copy)]
+#[cfg_attr(debug_assertions, derive(Debug))]
+pub enum Severity {
+    Success = 0x00,
+    Informational = 0x01,
+    Warning = 0x02,
+    Error = 0x03,
+}
+
+#[repr(u16)]
+#[derive(FromPrimitive, Clone, Copy)]
+#[cfg_attr(debug_assertions, derive(Debug))]
+pub enum Facility {
+    Debugger = 0x001,
+    RpcRuntime = 0x002,
+    RpcStubs = 0x003,
+    IoErrorCode = 0x004,
+    NtWin32 = 0x007,
+    NtSspi = 0x009,
+    TerminalServer = 0x00A,
+    MuiErrorCode = 0x00B,
+    UsbErrorCode = 0x10,
+    HidErrorCode = 0x011,
+    ClusterErrorCode = 0x013,
+    AcpiErrorCode = 0x014,
+    SxsErrorCode = 0x015,
+    Transaction = 0x019,
+    Commonlog = 0x01A,
+    Video = 0x01B,
+    FilterManager = 0x01C,
+    Monitor = 0x01D,
+    GraphicsKernel = 0x01E,
+    DriverFrameWork = 0x020,
+    FveErrorCode = 0x021,
+    FWPErrorCode = 0x022,
+    NdisErrorCode = 0x023,
+    Hypervisor = 0x035,
+    Ipsec = 0x036,
+    MaximumValue = 0x037,
 }
 
 #[repr(u32)]
