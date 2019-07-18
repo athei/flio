5281a1934081bfd7fdc3dda5662f7a5dd8a7a599
Alexander Theißen <alex@theissen.io>
2019-07-18 21:01:43 +0200

    Started with nom2 transition

--- smb2-packet/src/transport.rs
+++ smb2-packet/src/transport.rs
@@ -1,3 +1,11 @@
-use nom::*;
+use nom::IResult;
+use nom::bytes::streaming::{
+    tag, take
+};
+use nom::sequence::preceded;
 
-named!(pub get_payload, preceded!(tag!(b"\x00"), length_bytes!(be_u24)));
+pub fn get_payload(&[u8]) -> IResult<&[u8], &[u8]> {
+    preceded(tag(b"\x00"), length_)
+}
+
+//named!(pub get_payload, preceded!(tag(b"\x00"), length_bytes!(be_u24)));
